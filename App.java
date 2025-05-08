a70cb301ab80284ce67244c65835ff83b969f574
GGP113 <paulaag316@gmail.com>
2025-05-07 20:58:05 -0500

    variables def

--- App.java
+++ App.java
@@ -5,18 +5,40 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
-        String infProfesor, nombreMateria, momento1, momento2, momento3;
-        String emailProfesor, telefonoProfesor;
+        String infProfesor="", nombreMateria="", momento1="", momento2="", momento3="";
+        String emailProfesor="", telefonoProfesor="";
 
-        String plazoUno, plazoDos, plazoTres;
-        String diaMateria, jornadaMateria;
+        String plazoUno="", plazoDos="", plazoTres="";
+        String diaMateria="", jornadaMateria="";
         
-        int diaFechaUno, diaFechaDos, diaFechaTres, mesFechaUno, mesFechaDos, mesFechaTres;
-        int diaFechaUnoE, diaFechaDosE, diaFechaTresE, mesFechaUnoE, mesFechaDosE, mesFechaTresE;
-        int difM1d, difM2d, difM3d;
-        int difM1m, difM2m, difM3m;
+        int diaFechaUno=0, diaFechaDos=0, diaFechaTres=0, mesFechaUno=0, mesFechaDos=0, mesFechaTres=0;
+        int diaFechaUnoE=0, diaFechaDosE=0, diaFechaTresE=0, mesFechaUnoE=0, mesFechaDosE=0, mesFechaTresE=0;
+        int difM1d=0, difM2d=0, difM3d=0;
+        int difM1m=0, difM2m=0, difM3m=0;
 
         int perfil;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" \nIngrese 1 ó 2 de acuerdo con su perfil: \n \t1)Profesor \n \t2)Estudiante");
+
+        perfil = sc.nextInt();
+        
+        switch (perfil) {
+            case 1:
+                
+                break;
+
+            case 2:
+            
+            break;
+    
+            default:
+                break;
+        }
+
+
+
+        
 
         
 
