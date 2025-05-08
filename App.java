54d8fbfb056b1cefb4c1aa962009d0827d711697
GGP113 <paulaag316@gmail.com>
2025-05-07 20:50:33 -0500

    CronoClase Java

---
+++ App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        String infProfesor, nombreMateria, momento1, momento2, momento3;
+        String emailProfesor, telefonoProfesor;
+
+        String plazoUno, plazoDos, plazoTres;
+        String diaMateria, jornadaMateria;
+        
+        int diaFechaUno, diaFechaDos, diaFechaTres, mesFechaUno, mesFechaDos, mesFechaTres;
+        int diaFechaUnoE, diaFechaDosE, diaFechaTresE, mesFechaUnoE, mesFechaDosE, mesFechaTresE;
+        int difM1d, difM2d, difM3d;
+        int difM1m, difM2m, difM3m;
+
+        int perfil;
+
+        
+
+
+
+
+    }
+
+
+
+}
