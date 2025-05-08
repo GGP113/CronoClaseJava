b75bfc3ba9ed7368f059be27da2abf0d25ee5aac
GGP113 <paulaag316@gmail.com>
2025-05-07 22:44:51 -0500

    avance java

--- App.java
+++ App.java
@@ -8,13 +8,13 @@ public class App {
         String infProfesor="", nombreMateria="", momento1="", momento2="", momento3="";
         String emailProfesor="", telefonoProfesor="";
 
-        String plazoUno="", plazoDos="", plazoTres="";
+        int  plazoTres=0;
         String diaMateria="", jornadaMateria="";
         
         int diaFechaUno=0, diaFechaDos=0, diaFechaTres=0, mesFechaUno=0, mesFechaDos=0, mesFechaTres=0;
         int diaFechaUnoE=0, diaFechaDosE=0, diaFechaTresE=0, mesFechaUnoE=0, mesFechaDosE=0, mesFechaTresE=0;
-        int difM1d=0, difM2d=0, difM3d=0;
-        int difM1m=0, difM2m=0, difM3m=0;
+        int difM1d=0, difM2d=0, difM3d=0, plazoDos=0;
+        int difM1m=0, difM2m=0, difM3m=0, plazoUno=0;
 
         int perfil;
         Scanner sc = new Scanner(System.in);
@@ -39,6 +39,7 @@ public class App {
             telefonoProfesor=sc.nextLine();
 
             System.out.println("\nINGRESO INFORMACIÓN MATERIA");
+
             System.out.println("\nIndique el nombre de la materia: ");
             nombreMateria=sc.nextLine();
 
@@ -51,15 +52,114 @@ public class App {
 
             System.out.println("\nINGRESO DE ACTIVIDADES EVALUATIVAS: ");
 
+            System.out.println("\nMOMENTO EVALUATIVO 1");
+
             System.out.println("\nIngrese el tipo de evaluación del momento 1: ");
             momento1=sc.nextLine();
 
+            System.out.println("\nIngrese la fecha de entrega del momento 1:");
+            System.out.println("\nIndique el dia de la evaluación (en número): ");
 
+            diaFechaUno=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes de la evaluacion (en número) : ");
+
+            mesFechaUno=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIngrese el plazo para el desarrollo de la actividad en días : ");
+            plazoUno=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nMOMENTO EVALUATIVO 2");
+
+
+            System.out.println("\nIngrese el tipo de evaluación del momento 2: ");
+            momento2=sc.nextLine();
+
+            System.out.println("\nIngrese la fecha de entrega del momento 2:");
+            System.out.println("\nIndique el dia de la evaluación (en número): ");
+
+            diaFechaDos=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes de la evaluacion (en número) : ");
+
+            mesFechaDos=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIngrese el plazo para el desarrollo de la actividad en días : ");
+            plazoDos=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nMOMENTO EVALUATIVO 3");
+
+            System.out.println("\nIngrese el tipo de evaluación del momento 3: ");
+            momento3=sc.nextLine();
+
+            System.out.println("\nIngrese la fecha de entrega del momento 3:");
+            System.out.println("\nIndique el dia de la evaluación (en número): ");
+
+            diaFechaTres=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes de la evaluacion (en número) : ");
+
+            mesFechaTres=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIngrese el plazo para el desarrollo de la actividad en días : ");
+            plazoTres=sc.nextInt();
 
                 
                 break;
 
             case 2:
+
+            System.out.println("\nREGISTRO DE ENTREGA DE ACTIVIDADES");
+
+            System.out.println("\nIngrese la fecha en la que entrega la evaluación del momento 1: ");
+            System.out.println("\nIndique el dia  (en número) :");
+            diaFechaUnoE=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes (en número) :");
+            mesFechaUnoE=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIngrese la fecha en la que entrega la evaluación del momento 2: ");
+            System.out.println("\nIndique el dia  (en número) :");
+            diaFechaDosE=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes (en número) :");
+            mesFechaDosE=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIngrese la fecha en la que entrega la evaluación del momento 3: ");
+            System.out.println("\nIndique el dia  (en número) :");
+            diaFechaTresE=sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nIndique el mes (en número) :");
+            mesFechaTresE=sc.nextInt();
+            sc.nextLine();
+
+            difM1d= diaFechaUnoE-diaFechaUno;
+            difM2d= diaFechaDosE-diaFechaDos;
+            difM3d= diaFechaTresE-diaFechaTres;
+
+
+            difM1m=mesFechaUnoE-mesFechaUno;
+            difM2m= mesFechaDosE-mesFechaDos;
+            difM3m= mesFechaTresE-mesFechaTres;
+
+
+
+
+
+
             
             break;
     
